ea759a67172f1d283f76859875feb6965dc9dd5a
Ondřej Láznička <get@ondrejlaznicka.cz>
2024-01-12 18:27:40 +0100

    Init

---
+++ src/RelationManagers/RevisionRelationManager.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Ewwe\FilamentRevisionsPlugin\RelationManagers;
+
+use Filament\Actions\Action;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables\Table;
+
+class RevisionRelationManager extends RelationManager
+{
+    protected static string $relationship = 'revisions';
+
+    public function table(Table $table)
+    {
+        return $table
+            ->defaultSort('id', 'DESC')
+            ->columns($this->getColumnsForTable())
+            ->actions([
+                Action::make('Restore'),
+            ]);
+    }
+
+    protected function getColumnsForTable(): array
+    {
+        $columns = [];
+        foreach ($this->ownerRecord->getFillable() as $item) {
+            if( isset( $this->ownerRecord?->translatable) ) {
+
+            }
+        }
+
+        return $columns;
+    }
+}
